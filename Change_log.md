fb439118ca136aa7482748276cdb8af26dddfd3f
zachary4001 <1514289+zachary4001@users.noreply.github.com>
2026-05-25 10:37:58 +0200

    initial commit: project structure, config, .gitignore

---
+++ Change_log.md
@@ -0,0 +1,12 @@
+# Change Log  
+- updated 25.05.26  
+
+## General configuration changes  
+
+## Specific File or Process Step changes  
+- config.py  
+
+### cifar10_main.ipynb  
+
+### config.py  
+
---
+++ config.py
@@ -0,0 +1,28 @@
+# config.py
+# this config file is for the project Corporacion Favorita retail data
+# updated 04.05.2026
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Detect if running on Streamlit Cloud or locally
+IS_LOCAL = os.path.exists(r'Q:\scripts\projects\ComputerVision-Term9')
+if IS_LOCAL:
+    PROJECT_PATH = r'Q:\scripts\projects\ComputerVision-Term9'
+else:
+    PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# Paths
+DATA_PATH     = os.getenv('DATA_PATH',     os.path.join(PROJECT_PATH, 'data'))
+OUTPUT_PATH   = os.getenv('OUTPUT_PATH',   os.path.join(PROJECT_PATH, 'outputs'))
+EXPORTS_PATH  = os.getenv('EXPORTS_PATH',  os.path.join(PROJECT_PATH, 'exports'))
+
+# Dataset
+# FILE_NAME     = "timeseries_with_features.csv"
+# DATE_COLUMN   = "date"
+# TARGET_COLUMN = "unit_sales"
+
+# MLflow
+# MLFLOW_URI    = os.getenv('MLFLOW_URI', 'http://localhost:5000')
+# EXPERIMENT    = "favorita"
